62c322a62726a2da85f76ab7df38d938b431d967
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-18 23:30:28 -0300

    usando a nova api hooks nas pages do projeto

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { connect, useSelector } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import api from '../../services/api';
 import { formatPrice } from '../../util/format';
 import * as CartActions from '../../store/modules/cart/actions';
 import { ProductList } from './styles';
 
-function Home({ addToCartRequest }) {
+export default function Home() {
   const [products, setProducts] = useState([]);
   const amount = useSelector(state =>
-    state.cart.reduce((amount, product) => {
-      amount[product.id] = product.amount;
+    state.cart.reduce((sumAmount, product) => {
+      sumAmount[product.id] = product.amount;
 
-      return amount;
+      return sumAmount;
     }, {})
   );
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     async function loadProducts() {
@@ -32,7 +33,7 @@ function Home({ addToCartRequest }) {
   }, []);
 
   function handleAddProduct(id) {
-    addToCartRequest(id);
+    dispatch(CartActions.addToCartRequest(id));
   }
 
   return (
@@ -55,11 +56,3 @@ function Home({ addToCartRequest }) {
     </ProductList>
   );
 }
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(CartActions, dispatch);
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(Home);
